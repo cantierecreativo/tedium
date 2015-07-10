cd27628809122a46e12bfb677fd25b789cf8ee6d
Adam Jahn <ajjahn@gmail.com>
2015-07-10 10:22:30 -0400

    Support multiple args in capybara's original set

--- lib/tedium/capybara/node_element.rb
+++ lib/tedium/capybara/node_element.rb
@@ -6,14 +6,13 @@ module Tedium
         base.send(:alias_method, :set, :augmented_select)
       end
 
-      def augmented_select(value)
+      def augmented_select(*args)
         if tag_name == "select"
-          find(:option_with_value_or_label, value).select_option
+          find(:option_with_value_or_label, args[0]).select_option
         else
-          original_set(value)
+          original_set(*args)
         end
       end
     end
   end
 end
-
